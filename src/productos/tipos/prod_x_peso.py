7fc315745651326c4e145a597900d466db099f83
JoaquinViera341 <vierajoaquin3@gmail.com>
2026-05-29 12:02:55 -0300

    tipoproducto
    añadido tipo producto a prod x peso y x unidad

--- src/productos/tipos/prod_x_peso.py
+++ src/productos/tipos/prod_x_peso.py
@@ -6,12 +6,13 @@ Representa a los productos que se vender por peso como verdura, fiambre, etc"""
 class Prod_x_peso(Producto): 
     def __init__(self, nombre, precio, stock, stockMax, codigoBarras, umbralMinimo, marca):
         super().__init__(nombre, precio, stock, stockMax, codigoBarras, umbralMinimo, marca)
-    
+        self.__tipo_producto = "Peso"
+
     """Metodo para calucular el precio final de un producto
     recibe los kilos comprados y devuelve el precio final"""
 
     def precioFinal(self, kilos_comprados : float = 1) -> float: 
         return self.getPrecio() * kilos_comprados
     
- 
-    
+    def getTipoProducto(self):
+        return self.__tipo_producto
--- src/productos/tipos/prod_x_unidad.py
+++ src/productos/tipos/prod_x_unidad.py
@@ -7,13 +7,15 @@ class Prod_x_unidad(Producto):
     def __init__(self, nombre, precio, stock, stockMax, codigoBarras, umbralMinimo, marca, unid_x_paquete):
         super().__init__(nombre, precio, stock, stockMax, codigoBarras, umbralMinimo, marca)
         self.__unid_x_paquete = unid_x_paquete
+        self.__tipo_producto = "Paquete"
 
     def precioFinal(self, cantidad : int = 1) -> float:
         return self.__unid_x_paquete * self.getPrecio() * cantidad
     
+    """getters"""
+    
     def getUnid_X_Paquete(self):
         return self.__unid_x_paquete
     
-  
-        
-    
+    def getTipoProducto(self):
+        return self.__tipo_producto
